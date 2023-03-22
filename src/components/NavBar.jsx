abcc5e402c088c9c7f77767ff4efa069df50b6bf
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-03-22 21:39:52 +0100

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+function NavBar() {
+  const location = useLocation();
+  const linkStyle = {
+    backgoundColor: "red",
+    fontSize: "2.5rem",
+  };
+
+  return (
+    <div>
+      <h1>NavBar</h1>
+      {/* <div style={{ display: "flex", gap: "1em" }}>
+        <Link style={location.pathname === '/' ? linkStyle : null} to='/'>HomePage</Link>
+        <Link style={location.pathname === '/about' ? linkStyle : null} to='about' state={ "send this message to about page" }>About</Link>
+      </div> */}
+
+      <div style={{ display: "flex", gap: "1em" }}>
+        <NavLink to="/" style={({ isActive }) => (isActive ? linkStyle : null)}>
+          HomePage
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? linkStyle : null)}
+        >
+          About
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [paintings, setPaintings] = useState([]);
+  useEffect(() => {
+    const fetchPaintings = async () => {
+      try {
+        const response = await fetch(
+          "https://www.rijksmuseum.nl/api/en/collection?key=cQfuqm3K&ps=100https://www.rijksmuseum.nl/api/en/collection?key=cQfuqm3K&ps=100&imgonly=true"
+        );
+        const result = await response.json();
+        setPaintings(result.artObjects);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchPaintings();
+  }, []);
+  console.log(paintings);
+
+  return (
+    <div>
+      <h1>Home Page </h1>
+      <h2>All Paintings :</h2>
+      {paintings.map((paint) => {
+        return (
+          <div key={paint.id}>
+            <h3>{paint.title}</h3>
+            <img className="paint" src={paint.webImage.url} alt="" />
+            <p>{paint.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
