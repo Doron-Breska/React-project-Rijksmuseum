f659fa7c6444651fad291158e9d6466754815ec6
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-03-25 23:14:14 +0100

    fixed error with the function to create select options
    now it only creates the options once during the first fetch :)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,24 @@ function Home() {
   const [paintings, setPaintings] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectArtist, setSelectArtist] = useState("");
+  const [artistsSet, setArtistsSet] = useState(new Set());
 
   useEffect(() => {
     setLoading(true);
     const fetchPaintings = async () => {
       try {
         const response = await fetch(
-          `https://www.rijksmuseum.nl/api/en/collection?key=cQfuqm3K&toppieces=True&ps=10&imgonly=true&involvedMaker=${selectArtist}`
+          `https://www.rijksmuseum.nl/api/en/collection?key=cQfuqm3K&toppieces=True&ps=100&imgonly=true&involvedMaker=${selectArtist}`
         );
         const result = await response.json();
         setPaintings(result.artObjects);
+        if (artistsSet.size === 0) {
+          const newArtistsSet = new Set();
+          for (let paint of result.artObjects) {
+            newArtistsSet.add(paint.principalOrFirstMaker);
+          }
+          setArtistsSet(newArtistsSet);
+        }
         setLoading(false);
       } catch (error) {
         console.log("error", error);
@@ -25,14 +33,6 @@ function Home() {
   }, [selectArtist]);
   console.log("the results of the fetch", paintings);
 
-  ///creatig set of unique painters from the initial fetch
-  const artistsSet = new Set();
-  for (let paint of paintings) {
-    artistsSet.add(paint.principalOrFirstMaker);
-  }
-  const arrayArtists = Array.from(artistsSet);
-
-  /// function to take the select input value
   function handleSelect(value) {
     setSelectArtist(value.replace(" ", "+"));
     console.log("test of handleSelect updated value", value.replace(" ", "+"));
@@ -41,7 +41,7 @@ function Home() {
   return (
     <>
       {loading && <h1 className="text-center m-1">LOADING...</h1>}
-      <Filters onSelect={handleSelect} arrayArtists={arrayArtists} />
+      <Filters onSelect={handleSelect} arrayArtists={Array.from(artistsSet)} />
       <div className="paintings-container">
         <Cards paintings={paintings} />
       </div>
