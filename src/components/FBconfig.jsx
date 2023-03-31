c780dcd508d449bd4df9cbc5e346d704b98d54d9
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-03-31 17:02:34 +0200

    new comment function + new fire store function for storing the comments and showing them afterwards

--- src/components/FBconfig.jsx
+++ src/components/FBconfig.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyArd9Cx87AzX8kYLDLrpFN7kYnl_m4Zu0Y",
@@ -12,5 +13,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { auth };
+export { db };
+export { app };
--- src/components/ModalBack.jsx
+++ src/components/ModalBack.jsx
@@ -1,13 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
-//
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  getDocs,
+} from "firebase/firestore";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import { db } from "../components/FbConfig";
+import { query, where, onSnapshot } from "firebase/firestore";
+
 function ModalBack({ selectedPainting, show, handleClose }) {
+  const { isUserLogged } = useContext(AuthContext);
   const [fullscreen, setFullscreen] = useState("md - down");
+  const [inputValue, setInputValue] = useState("");
+  const [comments, setComments] = useState([]);
 
   function handleShow(breakpoint) {
     setFullscreen(breakpoint);
   }
-  console.log("test passing info to the modal", selectedPainting);
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  async function addComment(e) {
+    e.preventDefault();
+    const paintingId = selectedPainting.id;
+    const userId = isUserLogged.uid;
+    const commentText = inputValue;
+    const userName = isUserLogged.displayName;
+
+    const commentData = {
+      paintingId: paintingId,
+      userId: userId,
+      commentText: commentText,
+      userName: userName,
+      timestamp: serverTimestamp(),
+    };
+    try {
+      const docRef = await addDoc(collection(db, "comments"), commentData);
+      console.log("Comment added with ID: ", docRef.id);
+      alert("comment added");
+      setInputValue("");
+    } catch (e) {
+      console.error("Error adding comment: ", e);
+    }
+  }
+
+  async function fetchComments() {
+    const paintingId = selectedPainting.id;
+    const commentsRef = collection(db, "comments");
+    const commentsQuery = query(
+      commentsRef,
+      where("paintingId", "==", paintingId)
+    );
+    const unsubscribe = onSnapshot(commentsQuery, (querySnapshot) => {
+      const fetchedComments = [];
+      querySnapshot.forEach((doc) => {
+        fetchedComments.push({ id: doc.id, ...doc.data() });
+      });
+
+      // Sort comments by timestamp
+      fetchedComments.sort((a, b) => {
+        return a.timestamp - b.timestamp;
+      });
+
+      setComments(fetchedComments);
+    });
+
+    return unsubscribe;
+  }
+
+  useEffect(() => {
+    if (selectedPainting) {
+      fetchComments();
+    }
+  }, [selectedPainting]);
 
   return (
     <Modal
@@ -17,17 +89,34 @@ function ModalBack({ selectedPainting, show, handleClose }) {
       onShow={() => handleShow("md-down")}
     >
       <Modal.Header closeButton>
-        <Modal.Title>Comments related to</Modal.Title>
+        <Modal.Title>
+          Comments related to {selectedPainting && selectedPainting.title}
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>
-          {selectedPainting.title}
-          {/* {selectedPainting && selectedPainting.title} */}
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis sed
-          assumenda perferendis, aliquam commodi nam corporis officia. Voluptate
-          veniam voluptates quibusdam. Nesciunt, quas dolorum iste autem sit at
-          eius architecto!
-        </p>
+        <div id="msgs">
+          {comments.length === 0 && <h3>no comments yet</h3>}
+          {comments.map((comment) => (
+            <div key={comment.id} className="comment">
+              <div className="userName">{comment.userName}:</div>
+              <div className="commentText">{comment.commentText}</div>
+            </div>
+          ))}
+        </div>
+        <Form onSubmit={addComment}>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="Write something.."
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </Modal.Body>
     </Modal>
   );
