de6a4678a92679914f7582a7ab57f440d07572b5
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-04-12 14:50:21 +0200

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer className="d-flex justify-content-center">
-      Rijksmuseum Twenthe | Lasondersingel 129 | 7514 BP Enschede | 053 201 2000
+      <span className="footer-text">Rijksmuseum Twenthe</span>
+      <span className="footer-text">Lasondersingel 129 </span>
+      <span className="footer-text">7514 BP Enschede</span>
+      <span className="footer-text">053 201 2000</span>
     </footer>
   );
 }
--- src/pages/profile/ManageHistory.jsx
+++ src/pages/profile/ManageHistory.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ManageComments from "../../components/ManageComments";
 import ManageLikes from "../../components/ManageLikes";
-import { faSmileWink } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSmileWink } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ManageHistory() {
   return (
@@ -10,6 +10,10 @@ function ManageHistory() {
       <div className="manage-history-header">
         <h1 style={{ textAlign: "left" }}>Indecisive ?</h1>
         <h1 style={{ textAlign: "right" }}>No problem ^^</h1>
+      </div>
+      <div className="manage-history-header-2">
+        <h1 style={{ textAlign: "left" }}>Comments first,</h1>
+        <h1 style={{ textAlign: "right" }}>Likes below.</h1>
       </div>
       <ManageComments />
       <ManageLikes />
