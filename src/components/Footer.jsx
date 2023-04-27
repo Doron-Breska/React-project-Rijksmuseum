0414fc5931f53cbd735a6750fd158615037d3b6b
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-04-27 14:46:59 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ import { FaTwitterSquare } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="d-flex justify-content-center">
-      {/* <span className="footer-text">Rijksmuseum Twenthe</span>
-      <span className="footer-text">Lasondersingel 129 </span>
+      <span className="footer-text">Rijksmuseum </span>
+      {/* <span className="footer-text">Lasondersingel 129 </span>
       <span className="footer-text">7514 BP Enschede</span>
       <span className="footer-text">053 201 2000</span> */}
       <span className="footer-text">
